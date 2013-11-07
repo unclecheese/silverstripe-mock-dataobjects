a1423848d9c29072bc6c9c46e9588bc8b4db096f
Uncle Cheese <unclecheese@leftandmain.com>
2013-11-08 10:46:38 +1300

    Add check for dependency installation

--- _config.php
+++ _config.php
@@ -1,6 +1,8 @@
 <?php
 
-
+if(!class_exists("Faker\Generator")) {
+    throw new RuntimeException("The silverstripe-mock-dataobjects module requires the Faker PHP library. You can install it by running 'composer require fzaninotto/faker:1.1.*@dev' in your web root. If you installed this module via composer, make the directory fzaninotto/faker exists in vendor/.");
+}
 
 define('MOCK_DATAOBJECTS_DIR',basename(dirname(__FILE__)));
 $e = Config::inst()->get("DBField", "extensions");
