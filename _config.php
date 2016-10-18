e98adc1e570238ef462cb6a5f45e1e35a7e2631b
Aaron Carlino <unclecheese@leftandmain.com>
2016-10-18 14:35:04 +1300

    BUGFIX: use add_extension() instead of config updates

--- _config.php
+++ _config.php
@@ -5,17 +5,14 @@ if(!class_exists("Faker\Generator")) {
 }
 
 define('MOCK_DATAOBJECTS_DIR',basename(dirname(__FILE__)));
-$e = Config::inst()->get("DBField", "extensions");
-$e[] = "MockDBField";
-Config::inst()->update("DBField", "extensions", $e);
+
+Object::add_extension('DBField', 'MockDBField');
+
 foreach(SS_ClassLoader::instance()->getManifest()->getDescendantsOf("DBField") as $class) {
 	$mockClass = "Mock{$class}Field";
 	if(class_exists($mockClass)) {
-		$e = Config::inst()->get($class, "extensions");
-		$e[] = $mockClass;
-		Config::inst()->update($class, "extensions", $e);
+		Object::add_extension($class, $mockClass);
 	}
 }
-
 
 CMSMenu::remove_menu_item('MockChildrenController');
